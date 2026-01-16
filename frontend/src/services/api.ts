ad44fa4cbf1512a90346be7dae489edf7cecdff5
PhuQuoc2005 <tranphuquoc10c1@gmail.com>
2026-01-16 13:12:22 +0700

    doi lai thanh host

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_URL = 'https://collabsphere-backend-mk5g.onrender.com/api';
-const API_URL = 'http://localhost:8080/api';
+const API_URL = 'https://collabsphere-backend-mk5g.onrender.com/api';
+//const API_URL = 'http://localhost:8080/api';
 
 const api = axios.create({
     baseURL: API_URL,
